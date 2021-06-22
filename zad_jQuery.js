8f0dd7362596c800c7a0d6006aa2b4848da326bd
Kira <kiryusha-ok@yandex.ru>
2021-06-22 21:34:34 +0300

    Работа с jQuery

---
+++ zad_jQuery.js
@@ -0,0 +1,6 @@
+alert('Задание по JS с применением библиотеки jQuery!');
+setTimeout(function(){$('body').fadeOut('slow')}, 3000);
+
+//$('button').on('click', () => {
+//	$('p').fadeIn('slow');
+//});
