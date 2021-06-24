0caa8cc66406cb0d4adec64e2c37be0ee8205503
Kira <kiryusha-ok@yandex.ru>
2021-06-24 17:51:16 +0300

    Модальное окно

--- Modal.js
+++ Modal.js
@@ -17,3 +17,15 @@ $(function($) {
         alert('OGOGO!!!');
     });
 });
+
+function makeModal () {
+	str = '<div class="screen"><div class="modal"><h1>MODAL WINDOW!</h1><div>click me!</div></div></div>';
+	$('.container').append(modal);
+	$('.modal > div').on('click', function() {
+	$('.screen').remove();
+	});
+	let d = JSON.parse(data);
+	if (d.success) {
+		$('h1').html('SUCCESS');
+		$('p').('')
+}
