b8ef00016455726df8b868f658adb50a128fe1c4
Kira <kiryusha-ok@yandex.ru>
2021-06-24 17:48:17 +0300

    Работа с jQuery и календарем

--- jQuery/zad_jQuery.js
+++ jQuery/zad_jQuery.js
@@ -1,8 +1,10 @@
 alert('Задание по JS с применением библиотеки jQuery!');
 //setTimeout(function(){$('body').fadeOut('slow')}, 3000);
 
+
+
 $(function($) {
 	$('button').on('click', () => {
 		$('p').fadeOut('slow');
 	});
-});
+}); 
